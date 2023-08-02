04cf6505327b3f54915f0280227cae6c8adcfab7
IN HO HAN <ihan6@myseneca.ca>
2023-08-02 02:11:38 -0400

    updated navbar ui depending on logged in logged out status

--- components/MainNav.js
+++ components/MainNav.js
@@ -12,12 +12,20 @@ import { useState } from "react";
 import { searchHistoryAtom } from "@/store";
 import { useAtom } from "jotai";
 import { addToHistory } from "@/lib/userData";
+import { readToken, removeToken } from "@/lib/authenticate";
 
 export default function MainNav() {
+  let token = readToken();
   const router = useRouter();
   const [searchField, setSearchField] = useState();
   const [isExpanded, setIsExpanded] = useState(false);
   const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
+
+  function logout() {
+    setIsExpanded(false);
+    removeToken();
+    router.push("/login");
+  }
 
   async function submitForm(e) {
     e.preventDefault();
@@ -56,50 +64,79 @@ export default function MainNav() {
                   <span>Home</span>
                 </Nav.Link>
               </Link>
-              <Link href="/search" legacyBehavior passHref>
-                <Nav.Link
-                  onClick={toggleExpand}
-                  active={router.pathname === "/search"}
-                >
-                  <span>Advanced Search</span>
-                </Nav.Link>
-              </Link>
+              {token && (
+                <Link href="/search" legacyBehavior passHref>
+                  <Nav.Link
+                    onClick={toggleExpand}
+                    active={router.pathname === "/search"}
+                  >
+                    <span>Advanced Search</span>
+                  </Nav.Link>
+                </Link>
+              )}
+            </Nav>
+            <Nav className="ml-auto">
+              {!token && (
+                <Link href="/register" legacyBehavior passHref>
+                  <Nav.Link
+                    onClick={toggleExpand}
+                    active={router.pathname === "/register"}
+                  >
+                    <span>Register</span>
+                  </Nav.Link>
+                </Link>
+              )}
+              {!token && (
+                <Link href="/login" legacyBehavior passHref>
+                  <Nav.Link
+                    onClick={toggleExpand}
+                    active={router.pathname === "/login"}
+                  >
+                    <span>Login</span>
+                  </Nav.Link>
+                </Link>
+              )}
             </Nav>
             &nbsp;
-            <Form onSubmit={submitForm} className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                name="searchField"
-                aria-label="Search"
-                onChange={(e) => setSearchField(e.target.value)}
-              />
-              <Button variant="success" type="submit">
-                Search
-              </Button>
-            </Form>
+            {token && (
+              <Form onSubmit={submitForm} className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  name="searchField"
+                  aria-label="Search"
+                  onChange={(e) => setSearchField(e.target.value)}
+                />
+                <Button variant="success" type="submit">
+                  Search
+                </Button>
+              </Form>
+            )}
             &nbsp;
-            <Nav>
-              <NavDropdown title="User Name" id="basic-nav-dropdown">
-                <Link href="/favourites" legacyBehavior passHref>
-                  <NavDropdown.Item
-                    onClick={toggleExpand}
-                    active={router.pathname === "/favourites"}
-                  >
-                    Favourites
-                  </NavDropdown.Item>
-                </Link>
-                <Link href="/history" legacyBehavior passHref>
-                  <NavDropdown.Item
-                    onClick={toggleExpand}
-                    active={router.pathname === "/history"}
-                  >
-                    History
-                  </NavDropdown.Item>
-                </Link>
-              </NavDropdown>
-            </Nav>
+            {token && (
+              <Nav>
+                <NavDropdown title={token.userName} id="basic-nav-dropdown">
+                  <Link href="/favourites" legacyBehavior passHref>
+                    <NavDropdown.Item
+                      onClick={toggleExpand}
+                      active={router.pathname === "/favourites"}
+                    >
+                      Favourites
+                    </NavDropdown.Item>
+                  </Link>
+                  <Link href="/history" legacyBehavior passHref>
+                    <NavDropdown.Item
+                      onClick={toggleExpand}
+                      active={router.pathname === "/history"}
+                    >
+                      History
+                    </NavDropdown.Item>
+                  </Link>
+                  <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            )}
           </Navbar.Collapse>
         </Container>
       </Navbar>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 /*********************************************************************************
- *  WEB422 – Assignment 5
+ *  WEB422 – Assignment 6
  *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  *  No part of this assignment has been copied manually or electronically from any other source
  *  (including web sites) or distributed to other students.
  *
- *  Name: IN HO HAN Student ID: 106053218 Date: 2023/07/12
- *  Railway URL: https://museum-api-v2-production.up.railway.app/
+ *  Name: IN HO HAN Student ID: 106053218 Date: 2023/08/02
  *
  ********************************************************************************/
 
